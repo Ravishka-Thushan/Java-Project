0fc3103c978fc79793cfbd4a71303c8c754467bf
Ravishka-Thushan <ravishkathushan.dev@gmail.com>
2024-06-30 10:57:21 +0530

    Add files via upload

---
+++ Salary_Calculator/src/salary_calculator/Salary_Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salary_calculator;
+
+/**
+ *
+ * @author User
+ */
+public class Salary_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
